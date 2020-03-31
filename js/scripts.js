18f6f9b2bdf45a917b42775b5e99b86b22746e92
Ivan Lebedzeu <ivan.lebedzeu@gmail.com>
2020-03-31 15:16:39 -0700

    update if statement to make better logic of the application

--- js/scripts.js
+++ js/scripts.js
@@ -17,12 +17,14 @@ $(document).ready(function(){
     var symptoms = $("input:checkbox[name=symptoms]:checked").length;
     var skills = $("input:checkbox[name=copingSkills]:checked").length;
 
-    if (negative > 6 && symptoms > 3 && skills <= 3) { 
+    if (negative >= 6 && skills <= 3) { 
       $(".result1").show(); 
-    } else if ((negative <= 6 && negative > 1) && (symptoms <= 3 && symptoms > 1) && (skills > 3 && skills < 8)) {
+    } else if ((negative <= 5 && negative >= 2) && (skills >= 3 && skills >= 7)) {
       $(".result2").show();
-    } else {
+    } else if (negative <= 1 && skills >= 8) {
       $(".result3").show();
+    } else {
+      $(".result4").show();
     }
   });
  });
